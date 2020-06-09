7991a0cf48914e284d6f1702f9940acbc3781929
Pratik Soni <pratikabu@gmail.com>
2020-06-08 22:17:50 -0500

    Initial commit

---
+++ res/calculator.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+	$(".btnnum").on("click", function () {
+		$("#result").val($("#result").val() + $(this).val());
+	});
+
+	$(".btnfunc").on("click", function () {
+		if("C" == $(this).val()) {
+			$("#expressionDiv").text("");
+			$("#result").val("");
+		} else {
+			var expression = $("#result").val();
+			$("#expressionDiv").append(" " + expression);
+			if("=" == $(this).val()) {
+				evalExp($("#expressionDiv").text());
+			} else {
+				$("#expressionDiv").append(" " + $(this).val());
+				$("#result").val("");
+			}
+		}
+	});
+
+	$("#btnClear").on("click", function () {
+		$("#result").val("");
+		$("#result").focus();
+	});
+	$("#result").focus();
+});
+
+function evalExp(expression) {
+	var result = math.evaluate(expression);
+	$("#result").val(result);
+	log(expression, result);
+}
+
+function log(expression, result) {
+	var line = "<div class='logLine'>" + expression + " = " + "<span class='resultLog'>" + result + "</span>" + "</div>";
+	$("#historyDiv").append(line);
+}
