32199f498759ff0ef134da309f3adc7335340f72
Pratik Soni <pratikabu@gmail.com>
2020-06-09 22:03:39 -0500

    Converted to Revealing Module Pattern

--- res/calculator.js
+++ res/calculator.js
@@ -1,40 +1,53 @@
-$(document).ready(function () {
-	$(".btnnum").on("click", function () {
-		$("#result").val($("#result").val() + $(this).val());
-	});
+var plog = (function () {
+	function log(expression, result) {
+		var line = "<div class='logLine'><span class='expressionLog'>" + expression
+			+ "</span> = " + "<span class='resultLog'>" + result + "</span>" + "</div>";
+		$("#historyDiv").append(line);
+	}
 
-	$(".btnfunc").on("click", function () {
-		if("AC" == $(this).val()) {
-			$("#expressionDiv").text("");
-			$("#result").val("");
-		} else if("←" == $(this).val()) {
-			// backspace
-		} else {
-			var expression = $("#result").val();
-			$("#expressionDiv").append(" " + expression);
-			if("=" == $(this).val()) {
-				evalExp($("#expressionDiv").text());
-			} else {
-				$("#expressionDiv").append(" " + $(this).val());
+	return { log: log }
+})();
+
+var pcalc = (function () {
+	function evalExp(expression) {
+		var result = math.evaluate(expression);
+		$("#result").val(result);
+		plog.log(expression, result);
+	}
+
+	function init() {
+		$(".btnnum").on("click", function () {
+			$("#result").val($("#result").val() + $(this).val());
+		});
+
+		$(".btnfunc").on("click", function () {
+			if("AC" == $(this).val()) {
+				$("#expressionDiv").text("");
 				$("#result").val("");
+			} else if("←" == $(this).val()) {
+				// backspace
+			} else {
+				var expression = $("#result").val();
+				$("#expressionDiv").append(" " + expression);
+				if("=" == $(this).val()) {
+					evalExp($("#expressionDiv").text());
+				} else {
+					$("#expressionDiv").append(" " + $(this).val());
+					$("#result").val("");
+				}
 			}
-		}
-	});
+		});
 
-	$("#btnClear").on("click", function () {
-		$("#result").val("");
+		$("#btnClear").on("click", function () {
+			$("#result").val("");
+			$("#result").focus();
+		});
 		$("#result").focus();
-	});
-	$("#result").focus();
+	}
+
+	return { init: init }
+})();
+
+$(document).ready(function () {
+	pcalc.init();
 });
-
-function evalExp(expression) {
-	var result = math.evaluate(expression);
-	$("#result").val(result);
-	log(expression, result);
-}
-
-function log(expression, result) {
-	var line = "<div class='logLine'>" + expression + " = " + "<span class='resultLog'>" + result + "</span>" + "</div>";
-	$("#historyDiv").append(line);
-}
